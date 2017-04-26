641afd1a47aa1eb58984e6f54c2b34f273c2afdb
Grace Stuart <gstuart@gmail.com>
2017-04-26 14:38:57 -0700

    remove rental from route file

--- app/router.js
+++ app/router.js
@@ -9,7 +9,6 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('rental');
   this.route('trail');
   this.route('blog');
 });
